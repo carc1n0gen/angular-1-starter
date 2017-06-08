6eda71af2f97db3945408a2a11e74b40990718ea
Carson Evans <cevans@opentext.com>
2017-06-08 18:03:43 -0400

    Forgot to inject the http provider

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,11 @@
 const angular = require('angular');
 const angularRoute = require('angular-route');
+const angularResource = require('angular-resource');
 
 window.jQuery = window.$ = require('jquery');
 require('bootstrap-sass');
 
-angular.module('app', ['ngRoute'])
+angular.module('app', ['ngRoute', 'ngResource'])
     .config(($routeProvider) => {
         $routeProvider
             .when('/home', {
