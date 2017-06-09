abf1d734c2692e2fc657a78e00f4b529410a89a6
Carson Evans <cevans@opentext.com>
2017-06-09 11:28:32 -0400

    More icons please

--- src/js/components/HomePage.js
+++ src/js/components/HomePage.js
@@ -8,6 +8,11 @@ module.exports = {
                     {{msg}}
                 </div>
                 <span class="glyphicon glyphicon-user"></span>
+                <span class="glyphicon glyphicon-heart"></span>
+                <span class="glyphicon glyphicon-star"></span>
+                <span class="glyphicon glyphicon-signal"></span>
+                <span class="glyphicon glyphicon-home"></span>
+                <span class="glyphicon glyphicon-cog"></span>
             </div>
         </div>
     `
