a2c4e2fdf0e5fc608304cd5078d29cecae5012c4
Carson Evans <carsonwevans@gmail.com>
2017-08-01 21:29:42 -0400

    Combined features in to one module

--- src/app.js
+++ src/app.js
@@ -6,8 +6,7 @@ import './app.scss';
 import routing from './app.config';
 import services from './services';
 import components from './components';
-import home from './features/home';
-import welcome from './features/welcome';
+import features from './features';
 
-angular.module('app', [ngRoute, ngResource, services, components, home, welcome])
+angular.module('app', [ngRoute, ngResource, services, components, features])
     .config(routing);
